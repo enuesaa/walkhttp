6c6a4443f68653a2cf29fa69d72149b22eaa770c
enuesaa <nsrciog@gmail.com>
2023-08-13 16:56:56 +0900

    feat: define dir structure

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+    "fmt"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("a")
+    flag.Parse()
+    args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("Required argument missing: WORKDIR")
+		return
+	}
+
+	workdir := args[0]
+	fmt.Println(workdir)
+
+    app := fiber.New()
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World 👋!")
+    })
+    app.Listen(":3000")
 }
