c1a2b10eab2f292f1b8b028b95fef81b232fb830
enuesaa <nsrciog@gmail.com>
2023-08-20 15:25:23 +0900

    feat: try to use graph-gophers/graphql-go

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/gofiber/fiber/v2 v2.48.0
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/graph-gophers/graphql-go v1.5.0 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- main.go
+++ main.go
@@ -1,31 +1,55 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	// "flag"
+	// "fmt"
 
-	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/fiber/v2"
+	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+	"net/http"
 )
 
+type query struct{}
+
+func (_ *query) Hey() string { return "Hey, world!" }
+
 func main() {
-	flag.Parse()
-	args := flag.Args()
-	if len(args) == 0 {
-		fmt.Println("Required argument missing: WORKDIR")
-		return
-	}
+	// flag.Parse()
+	// args := flag.Args()
+	// if len(args) == 0 {
+	// 	fmt.Println("Required argument missing: WORKDIR")
+	// 	return
+	// }
 
-	workdir := args[0]
-	fmt.Println(workdir)
+	// workdir := args[0]
+	// fmt.Println(workdir)
 
-	app := fiber.New()
+	// app := fiber.New()
 
 	// _ := app.Group("/api")
 	// create api routes here
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		// should return html or assets
-		return c.SendString("")
-	})
-	app.Listen(":3000")
+	s := `
+	type Query {
+		hey: String!
+	}
+	`
+
+	schema := graphql.MustParseSchema(s, &query{})
+
+	// app.Post("/query", func(c *fiber.Ctx) error {
+	// 	handler := &relay.Handler{Schema: schema}
+	// 	handler.ServeHTTP(c.Response(), c.Request())
+	// 	c.SendString()
+	// 	return nil
+	// })
+	http.Handle("/query", &relay.Handler{Schema: schema})
+	http.ListenAndServe(":3000", nil)
+
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	// should return html or assets
+	// 	return c.SendString("")
+	// })
+	// app.Listen(":3000")
 }
