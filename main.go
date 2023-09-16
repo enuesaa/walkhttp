59a230dd9a7c4eec3527f2cfcd9b89515409e97f
enuesaa <nsrciog@gmail.com>
2023-09-17 02:55:10 +0900

    feat: customize error handler to return error response

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	_ "embed"
+	"errors"
 	"fmt"
 	"log"
 	"os"
-	_ "embed"
 
 	"github.com/enuesaa/walkin/controller"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 //go:embed build/index.html
 var indexHtmlBytes []byte
 //go:embed build/index.js
 var indexJsBytes []byte
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+func errorHandler(ctx *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+	var e *fiber.Error
+	if errors.As(err, &e) {
+		code = e.Code
+	}
+	return ctx.Status(code).JSON(ErrorResponse { Error: "Internal Sever Error" })
+}
 
 func main() {
 	workdir, err := os.Getwd()
@@ -23,7 +37,10 @@ func main() {
 	fmt.Printf("running..\n\n")
 	fmt.Printf("workdir: %s\n", workdir)
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: errorHandler,
+	})
+	app.Use(recover.New())
 
 	queryCtl := controller.QueryController{}
 	app.Post("/query", queryCtl.Query)
