327a450ead9743dfab720c47b2af45cdd2e340bd
enuesaa <nsrciog@gmail.com>
2023-10-09 23:13:20 +0900

    feat(cli): use cobra

--- go.mod
+++ go.mod
@@ -8,11 +8,14 @@ require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/graph-gophers/graphql-go v1.5.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/enuesaa/walkin/controller"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/spf13/cobra"
 )
 
 type ErrorResponse struct {
@@ -26,36 +27,49 @@ func errorHandler(ctx *fiber.Ctx, err error) error {
 }
 
 func main() {
-	workdir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
+	cmd := &cobra.Command{
+		Use:     "walkin",
+		Short:   "Instant web server.",
+		Version: "0.0.1",
+		Run: func(cmd *cobra.Command, args []string) {
+			workdir, err := os.Getwd()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("running on %s\n", workdir)
+				
+			app := fiber.New(fiber.Config{
+				ErrorHandler: errorHandler,
+			})
+			app.Use(recover.New())
+		
+			queryCtl := controller.QueryController{}
+			app.Post("/query", queryCtl.Query)
+		
+			app.Get("/", func(c *fiber.Ctx) error {
+				f, err := os.Open("./testdata/index.html")
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+		
+				content, err := io.ReadAll(f)
+				if err != nil {
+					return err
+				}
+				c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+		
+				return c.SendString(string(content))
+			})
+		
+			app.Listen(":3000")
+		},
 	}
-	fmt.Printf("running..\n\n")
-	fmt.Printf("workdir: %s\n", workdir)
 
-	app := fiber.New(fiber.Config{
-		ErrorHandler: errorHandler,
-	})
-	app.Use(recover.New())
+	// disable default
+	cmd.SetHelpCommand(&cobra.Command{Hidden: true})
+	cmd.PersistentFlags().BoolP("help", "", false, "Show help information")
+	cmd.CompletionOptions.DisableDefaultCmd = true
 
-	queryCtl := controller.QueryController{}
-	app.Post("/query", queryCtl.Query)
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		f, err := os.Open("./testdata/index.html")
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-
-		content, err := io.ReadAll(f)
-		if err != nil {
-			return err
-		}
-		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
-
-		return c.SendString(string(content))
-	})
-
-	app.Listen(":3000")
+	cmd.Execute()
 }
