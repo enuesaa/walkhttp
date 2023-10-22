d07499384b5f574c88912cf7c4790e9ba7a6bf0d
enuesaa <nsrciog@gmail.com>
2023-10-22 15:24:19 +0900

    feat: カレントディレクトリを serve できるようにした

--- internal/cli/serve.go
+++ internal/cli/serve.go
@@ -1,7 +1,11 @@
 package cli
 
 import (
-	_ "github.com/enuesaa/walkin/internal/handler"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cobra"
 )
 
@@ -10,8 +14,28 @@ func CreateServeCmd() *cobra.Command {
 		Use:   "serve",
 		Short: "serve instant web server.",
 		Run: func(cmd *cobra.Command, args []string) {
-			// config, _ := cmd.Flags().GetString("config")
-			// handler.Handle(port)
+			workdir, _ := os.Getwd()
+			fmt.Printf("running on %s\n", workdir)
+		
+			app := fiber.New()
+			app.Get("/*", func(c *fiber.Ctx) error {
+				path := c.OriginalURL() // like `/`
+
+				f, err := os.Open(fmt.Sprintf(".%s", path))
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+
+				content, err := io.ReadAll(f)
+				if err != nil {
+					return err
+				}
+				// c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+
+				return c.SendString(string(content))
+			})
+			app.Listen(":3000")
 		},
 	}
 	cmd.Flags().String("config", "config.json", "config file path")
