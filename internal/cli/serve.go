998ff4a221e58f31830197f8dd760b65510dd44a
enuesaa <nsrciog@gmail.com>
2023-10-28 19:56:14 +0900

    feat(serve): enable to specify port

--- internal/cli/serve.go
+++ internal/cli/serve.go
@@ -15,7 +15,9 @@ func CreateServeCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "serve",
 		Short: "serve static files",
-		Run: func(cmd *cobra.Command, args []string) {		
+		Run: func(cmd *cobra.Command, args []string) {	
+			port, _ := cmd.Flags().GetInt("port")
+	
 			app := fiber.New()
 			app.Get("/*", func(c *fiber.Ctx) error {
 				requestPath := c.Path() // like `/`
@@ -37,12 +39,16 @@ func CreateServeCmd() *cobra.Command {
 				}
 				mimeType := http.DetectContentType(content)
 				c.Set(fiber.HeaderContentType, mimeType)
+				fmt.Printf("%s is %s.", path, mimeType)
 
 				return c.SendString(string(content))
 			})
-			app.Listen(":3000")
+
+			addr := fmt.Sprintf(":%d", port)
+			app.Listen(addr)
 		},
 	}
+	cmd.Flags().Int("port", 3000, "port")
 
 	return cmd
 }
--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"github.com/enuesaa/walkin/internal/cli"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	app := cli.CreateApp()
+	app := &cobra.Command{
+		Use:     "walkin",
+		Short:   "Instant web server.",
+		Version: "0.0.1",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	app.AddCommand(cli.CreateServeCmd())
+	app.AddCommand(cli.CreateServemdCmd())
+
+	// disable default
+	app.SetHelpCommand(&cobra.Command{Hidden: true})
+	app.PersistentFlags().BoolP("help", "", false, "Show help information")
+	app.CompletionOptions.DisableDefaultCmd = true
+
 	app.Execute()
 }
