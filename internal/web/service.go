3118d314f89a067487ff6e0c971215ec94fdbd1a
nua <enuesaa@gmail.com>
2023-11-19 19:54:39 +0900

    feat(localfiles): trim basepath

--- internal/web/service.go
+++ internal/web/service.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/proxy"
+	"strings"
+
 	"github.com/enuesaa/walkin/internal/repository"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
 )
 
 type WebService struct {
@@ -38,32 +40,46 @@ func (srv *WebService) calcAddress() string {
 func (srv *WebService) Serve() {
 	app := fiber.New()
 
+	fmt.Printf("%+v\n", srv.serveConfig)
+
 	for path, behavior := range srv.serveConfig.Paths {
 		if behavior.Behavior == Proxy {
 			app.Get(path, proxy.Forward(behavior.ProxyConfig.Url))
 		}
 		if behavior.Behavior == ReadLocalFiles {
-			app.Get(path, func(c *fiber.Ctx) error {
-				requestPath := c.Path() // like `/`
-		
-				path := fmt.Sprintf(".%s", requestPath)
-				if path == "./" {
-					path = "./index.html"
-				}
-				f, err := os.Open(path)
-				if err != nil {
-					return err
-				}
-				defer f.Close()
-		
-				content, err := io.ReadAll(f)
-				if err != nil {
-					return err
-				}
-				return c.SendString(string(content))
-			})		
+			fmt.Printf("path is %s%+v\n", path, behavior)
+
+			app.Get(path, createReadLocalFilesHandler(path))		
 		}
 	}
 
 	app.Listen(srv.calcAddress())
+}
+
+func createReadLocalFilesHandler(path string) func(c *fiber.Ctx) error {
+	handler := func(c *fiber.Ctx) error {
+		requestPath := c.Path() // like `/`
+
+		basePath := strings.Replace(path, "*", "", -1)
+		lookupPath := fmt.Sprintf("./%s", strings.TrimPrefix(requestPath, basePath))
+
+		if strings.HasSuffix(lookupPath, "/") {
+			lookupPath = lookupPath + "index.html"
+		}
+		fmt.Printf("lookupPath is %s\n", lookupPath)
+
+		f, err := os.Open(lookupPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		content, err := io.ReadAll(f)
+		if err != nil {
+			return err
+		}
+		return c.SendString(string(content))
+	}
+
+	return handler
 }
