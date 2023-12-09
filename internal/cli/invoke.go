43f5152d93a4852fad992d7f10aff33cfe86a60b
nua <enuesaa@gmail.com>
2023-12-09 23:20:59 +0900

    chore: stash old files to proceed developing

--- internal/cli/invoke.go
+++ internal/cli/invoke.go
@@ -1,11 +1,6 @@
 package cli
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-
 	"github.com/enuesaa/walkin/internal/repository"
 	"github.com/spf13/cobra"
 )
@@ -15,20 +10,6 @@ func CreateInvokeCmd(repos repository.Repos) *cobra.Command {
 		Use:   "invoke",
 		Short: "invoke",
 		Run: func(cmd *cobra.Command, args []string) {	
-			req, err := http.NewRequest("GET", "https://example.com/", nil)
-			if err != nil {
-				log.Fatalf("error: %s", err.Error())
-			}
-
-			client := http.Client{}
-			res, err := client.Do(req)
-			if err != nil {
-				log.Fatalf("error: %s", err.Error())
-			}
-			defer res.Body.Close()
-
-			body, _ := io.ReadAll(res.Body)
-			fmt.Println(string(body))
 		},
 	}
 
--- internal/cli/mock.go
+++ internal/cli/mock.go
@@ -1,8 +1,6 @@
 package cli
 
 import (
-	"fmt"
-
 	"github.com/enuesaa/walkin/internal/repository"
 	"github.com/spf13/cobra"
 )
@@ -12,16 +10,10 @@ func CreateMockCmd(repos repository.Repos) *cobra.Command {
 		Use:   "mock",
 		Short: "mock",
 		Run: func(cmd *cobra.Command, args []string) {	
-			portFlag, _ := cmd.Flags().GetInt("port")
-			fmt.Printf("%d\n", portFlag)
-
 			// read mock files.
-
-			fmt.Printf("mocking endpoints below.\n")
 			// list endpoints
 		},
 	}
-	cmd.Flags().Int("port", 3000, "port")
 
 	return cmd
 }
