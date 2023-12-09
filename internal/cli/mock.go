ccce5d2b7b343dcbd0a3c1dcdfc5f6bf5ec75e28
nua <enuesaa@gmail.com>
2023-12-09 23:59:20 +0900

    feat(mock): serve mock server on mock command.

--- internal/cli/mock.go
+++ internal/cli/mock.go
@@ -1,7 +1,13 @@
 package cli
 
 import (
+	"fmt"
+	"mime"
+	"path/filepath"
+	"strings"
+
 	"github.com/enuesaa/walkin/internal/repository"
+	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cobra"
 )
 
@@ -10,8 +16,24 @@ func CreateMockCmd(repos repository.Repos) *cobra.Command {
 		Use:   "mock",
 		Short: "mock",
 		Run: func(cmd *cobra.Command, args []string) {	
-			// read mock files.
-			// list endpoints
+			app := fiber.New()
+			app.Get("/*", func(c *fiber.Ctx) error {
+				path := c.Path() // like `/`
+				path = fmt.Sprintf("dist%s", path)
+				if strings.HasSuffix(path, "/") {
+					path += "index.html"
+				}
+
+				// read file here.
+
+				fileExt := filepath.Ext(path)
+				mimeType := mime.TypeByExtension(fileExt)
+				c.Set(fiber.HeaderContentType, mimeType)
+
+				return c.SendString("")
+			})
+
+			app.Listen(":3000")
 		},
 	}
 
