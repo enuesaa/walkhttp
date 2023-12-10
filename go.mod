08e0fbf3e1dd53911e7f58d4640a4eee497772f0
nua <enuesaa@gmail.com>
2023-12-10 23:55:42 +0900

    feat: try jaeger

--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.21.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.51.0
+	github.com/opentracing/opentracing-go v1.2.0
 	github.com/spf13/cobra v1.7.0
+	github.com/stretchr/testify v1.8.4
+	github.com/uber/jaeger-client-go v2.30.0+incompatible
 )
 
 require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.4.0 // indirect
@@ -16,14 +20,17 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/cli/invoke.go
+++ pkg/cli/invoke.go
@@ -1,13 +1,14 @@
 package cli
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"net/http"
+	"time"
 
 	"github.com/enuesaa/walkin/pkg/repository"
+	"github.com/opentracing/opentracing-go"
 	"github.com/spf13/cobra"
+	"github.com/uber/jaeger-client-go"
+	jaegerConfig "github.com/uber/jaeger-client-go/config"
 )
 
 func CreateInvokeCmd(repos repository.Repos) *cobra.Command {
@@ -15,23 +16,52 @@ func CreateInvokeCmd(repos repository.Repos) *cobra.Command {
 		Use:   "invoke",
 		Short: "invoke",
 		Run: func(cmd *cobra.Command, args []string) {	
-			url, _ := cmd.Flags().GetString("url")
+			// url, _ := cmd.Flags().GetString("url")
 
-			res, err := http.Get(url)
-			if err != nil {
-				log.Fatalf("Error: %s\n", err)
-			}
-			defer res.Body.Close()
+			// res, err := http.Get(url)
+			// if err != nil {
+			// 	log.Fatalf("Error: %s\n", err)
+			// }
+			// defer res.Body.Close()
 
-			resbodybytes, err := io.ReadAll(res.Body)
-			if err != nil {
-				log.Fatalf("Error: %s\n", err)
+			// resbodybytes, err := io.ReadAll(res.Body)
+			// if err != nil {
+			// 	log.Fatalf("Error: %s\n", err)
+			// }
+			// resbody := string(resbodybytes)
+			// fmt.Printf("%s\n", resbody)
+
+			// see https://qiita.com/ike_dai/items/f7d95852a86a46e1f19d
+
+			cfg := jaegerConfig.Configuration{
+				Sampler: &jaegerConfig.SamplerConfig{
+					Type:  "const",
+					Param: 1,
+				},
+				Reporter: &jaegerConfig.ReporterConfig{
+					LogSpans:            true,
+					BufferFlushInterval: 1 * time.Second,
+					LocalAgentHostPort:  "127.0.0.1:5775",
+				},
 			}
-			resbody := string(resbodybytes)
-			fmt.Printf("%s\n", resbody)
+			tracer, closer, err := cfg.New(
+				"sample",
+				jaegerConfig.Logger(jaeger.StdLogger),
+			)
+			if err != nil {
+				log.Fatalf("Erraor: %s\n", err.Error())
+			}
+			opentracing.SetGlobalTracer(tracer)
+			defer closer.Close()
+
+			parent := opentracing.GlobalTracer().StartSpan("hello")
+			defer parent.Finish()
+			child := opentracing.GlobalTracer().StartSpan(
+			"world", opentracing.ChildOf(parent.Context()))
+			defer child.Finish()
 		},
 	}
-	cmd.Flags().String("url", "https://example.com", "invoke url")
+	// cmd.Flags().String("url", "https://example.com", "invoke url")
 
 	return cmd
 }
