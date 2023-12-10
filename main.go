b2aacb7030ee63a749879a0a38029b8060b47c50
nua <enuesaa@gmail.com>
2023-12-10 23:11:58 +0900

    feat: add help message template of cli

--- main.go
+++ main.go
@@ -7,20 +7,37 @@ import (
 )
 
 func main() {
-	repos := repository.NewRepos()
-
 	app := &cobra.Command{
 		Use:     "walkin",
-		Short:   "Instant web server.",
-		Version: "0.0.1",
+		Short:   "Instant web server to mock web api.",
+		Version: "0.0.2",
 	}
+
+	repos := repository.NewRepos()
 	app.AddCommand(cli.CreateMockCmd(repos))
 	app.AddCommand(cli.CreateInvokeCmd(repos))
 
 	// disable default
 	app.SetHelpCommand(&cobra.Command{Hidden: true})
-	app.PersistentFlags().BoolP("help", "", false, "Show help information")
 	app.CompletionOptions.DisableDefaultCmd = true
+	app.SilenceUsage = true
+	app.PersistentFlags().SortFlags = false
+	app.PersistentFlags().BoolP("help", "", false, "Show help information")
+	app.PersistentFlags().BoolP("version", "", false, "Show version")
+	app.SetHelpTemplate(`{{.Short}}
+{{if .HasAvailableSubCommands}}{{$cmds := .Commands}}{{if eq (len .Groups) 0}}
+Available Commands:{{range $cmds}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{else}}{{range $group := .Groups}}
+
+{{.Title}}{{range $cmds}}{{if (and (eq .GroupID $group.ID) (or .IsAvailableCommand (eq .Name "help")))}}
+{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{end}}
+{{end}}
+{{if .HasAvailableLocalFlags}}Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+{{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}
+`)
 
 	app.Execute()
 }
