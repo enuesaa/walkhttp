9532aabe2cf28e04d3847e625dea33085fea8c56
nua <enuesaa@gmail.com>
2023-12-10 20:05:19 +0900

    feat(mock): read local file

--- internal/cli/mock.go
+++ internal/cli/mock.go
@@ -3,8 +3,8 @@ package cli
 import (
 	"fmt"
 	"mime"
+	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/enuesaa/walkin/internal/repository"
 	"github.com/gofiber/fiber/v2"
@@ -17,22 +17,23 @@ func CreateMockCmd(repos repository.Repos) *cobra.Command {
 		Short: "mock",
 		Run: func(cmd *cobra.Command, args []string) {	
 			app := fiber.New()
+
 			app.Get("/*", func(c *fiber.Ctx) error {
 				path := c.Path() // like `/`
-				path = fmt.Sprintf("dist%s", path)
-				if strings.HasSuffix(path, "/") {
-					path += "index.html"
-				}
+				path = filepath.Join("./", path, "index.html")
+				fmt.Printf("%s", path)
 
-				// read file here.
+				f, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
 
 				fileExt := filepath.Ext(path)
 				mimeType := mime.TypeByExtension(fileExt)
 				c.Set(fiber.HeaderContentType, mimeType)
 
-				return c.SendString("")
+				return c.SendString(string(f))
 			})
-
 			app.Listen(":3000")
 		},
 	}
