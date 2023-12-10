9de6e462718160e51b4a75a38ac7a48925cf5235
nua <enuesaa@gmail.com>
2023-12-10 23:38:47 +0900

    feat: try opentracing

--- pkg/cli/invoke.go
+++ pkg/cli/invoke.go
@@ -1,6 +1,11 @@
 package cli
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"github.com/enuesaa/walkin/pkg/repository"
 	"github.com/spf13/cobra"
 )
@@ -10,8 +15,23 @@ func CreateInvokeCmd(repos repository.Repos) *cobra.Command {
 		Use:   "invoke",
 		Short: "invoke",
 		Run: func(cmd *cobra.Command, args []string) {	
+			url, _ := cmd.Flags().GetString("url")
+
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatalf("Error: %s\n", err)
+			}
+			defer res.Body.Close()
+
+			resbodybytes, err := io.ReadAll(res.Body)
+			if err != nil {
+				log.Fatalf("Error: %s\n", err)
+			}
+			resbody := string(resbodybytes)
+			fmt.Printf("%s\n", resbody)
 		},
 	}
+	cmd.Flags().String("url", "https://example.com", "invoke url")
 
 	return cmd
 }
