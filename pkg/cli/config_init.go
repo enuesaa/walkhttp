b8e7145e5cb863b4ddabc31d53e5414f01f6a981
nua <enuesaa@gmail.com>
2023-12-17 19:47:28 +0900

    feat: create config file

--- pkg/cli/config_init.go
+++ pkg/cli/config_init.go
@@ -1,16 +1,32 @@
 package cli
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/enuesaa/walkin/pkg/repository"
 	"github.com/spf13/cobra"
 )
+
+type ConfigFileResource struct {
+	Url string `json:"url"`
+}
+
+type ConfigFile struct {
+	Resources map[string]ConfigFileResource
+}
 
 func CreateConfigInitCmd(repos repository.Repos) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "init",
 		Short: "create config file with prompt",
 		Run: func(cmd *cobra.Command, args []string) {
-
+			config := ConfigFile{}
+			fbyte, err := json.Marshal(config)
+			if err != nil {
+				log.Fatalf("Error: %s\n", err.Error())
+			}
+			repos.Fs.Create("config.json", fbyte)
 		},
 	}
 
