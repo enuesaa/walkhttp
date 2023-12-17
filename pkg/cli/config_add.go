d0ca631e324086d7b9c26d4a1b4aa3710aeec3f9
nua <enuesaa@gmail.com>
2023-12-17 19:56:34 +0900

    feat(config): read config file

--- pkg/cli/config_add.go
+++ pkg/cli/config_add.go
@@ -1,6 +1,10 @@
 package cli
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+
 	"github.com/enuesaa/walkin/pkg/repository"
 	"github.com/spf13/cobra"
 )
@@ -9,6 +13,19 @@ func CreateConfigAddCmd(repos repository.Repos) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "add",
 		Short: "add new path to config file.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fbyte, err := repos.Fs.Read("config.json")
+			if err != nil {
+				log.Fatalf("Error: %s\n", err.Error())
+			}
+
+			var config ConfigFile
+			if err := json.Unmarshal(fbyte, &config); err != nil {
+				log.Fatalf("Errpr: %s\n", err.Error())
+			}
+
+			fmt.Printf("%+v\n", config)
+		},
 	}
 
 	return cmd
