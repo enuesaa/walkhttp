77279a73e529698a12ab1f9b4a95fb0328a33659
nua <enuesaa@gmail.com>
2023-12-31 13:05:17 +0900

    feat: move serve logic under usecase

--- pkg/cli/serve.go
+++ pkg/cli/serve.go
@@ -1,13 +1,10 @@
 package cli
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/enuesaa/walkin/pkg/invoke"
 	"github.com/enuesaa/walkin/pkg/repository"
 	"github.com/enuesaa/walkin/pkg/usecase"
-	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cobra"
 )
 
@@ -22,32 +19,7 @@ func CreateServeCmd(repos repository.Repos) *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			workdir, _ := cmd.Flags().GetString("workdir")
 
-			pages, err := usecase.ListPages(repos, workdir)
-			if err != nil {
-				log.Fatalf("Error: %s\n", err.Error())
-			}
-
-			app := fiber.New()
-
-			for _, page := range pages {
-				fmt.Printf("found: %s\n", page.Filename)
-
-				route := fmt.Sprintf("/%s", page.Filename)
-				app.Get(route, func(c *fiber.Ctx) error {
-					invokeSrv := invoke.NewInvokeSrv(repos)
-					req := invoke.Request{
-						Method: page.Method,
-						Url: page.Url,
-					}
-					res, err := invokeSrv.Invoke(req)
-					if err != nil {
-						return err
-					}
-					return c.SendString(string(res.Body))
-				})
-			}
-
-			if err := app.Listen(":3000"); err != nil {
+			if err := usecase.Serve(repos, workdir); err != nil {
 				log.Fatalf("Error: %s\n", err.Error())
 			}
 		},
@@ -57,10 +29,3 @@ func CreateServeCmd(repos repository.Repos) *cobra.Command {
 	return cmd
 }
 
-// command := exec.Command("printf", `{"message": "%s"}`, "hello")
-// result, err := command.Output()
-// if err != nil {
-// 	return err
-// }
-// var message TriggerResult
-// json.Unmarshal(result, &message)
--- pkg/usecase/serve.go
+++ pkg/usecase/serve.go
@@ -1,2 +1,38 @@
 package usecase
 
+import (
+	"fmt"
+
+	"github.com/enuesaa/walkin/pkg/invoke"
+	"github.com/enuesaa/walkin/pkg/repository"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Serve(repos repository.Repos, workdir string) error {
+	pages, err := ListPages(repos, workdir)
+	if err != nil {
+		return err
+	}
+
+	app := fiber.New()
+	for _, page := range pages {
+		route := fmt.Sprintf("/%s", page.Filename)
+		app.Get(route, func(c *fiber.Ctx) error {
+			invokeSrv := invoke.NewInvokeSrv(repos)
+			req := invoke.Request{
+				Method: page.Method,
+				Url: page.Url,
+			}
+			res, err := invokeSrv.Invoke(req)
+			if err != nil {
+				return err
+			}
+			return c.SendString(string(res.Body))
+		})
+	}
+
+	if err := app.Listen(":3000"); err != nil {
+		return err
+	}
+	return nil
+}
