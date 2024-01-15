645a797af499c16dbe61d94ebcf47bb87ed53faa
nua <enuesaa@gmail.com>
2024-01-16 00:52:16 +0900

    feat(sync); download files

--- pkg/cli/sync.go
+++ pkg/cli/sync.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -84,16 +85,38 @@ func CreateSyncCmd(repos repository.Repos) *cobra.Command {
 			}
 
 			if isClient {
-				res, err := http.Get(host)
+				listres, err := http.Get(fmt.Sprintf("http://%s/files", host))
 				if err != nil {
 					log.Fatalf("Error: %s", err.Error())
 				}
-				defer res.Body.Close()
-				fmt.Printf("status: %s", res.Status)
-				bodybyte, err := io.ReadAll(res.Body)
-				repos := repository.NewRepos()
-				if err := repos.Fs.Create("aaa.json", bodybyte); err != nil {
+				defer listres.Body.Close()
+				fmt.Printf("status(%s): %s", listres.Request.URL, listres.Status)
+				fbytes, err := io.ReadAll(listres.Body)
+				var files SyncFileList
+				if err := json.Unmarshal(fbytes, &files); err != nil {
 					log.Fatalf("Error: %s", err.Error())
+				}
+
+				for _, file := range files.Items {
+					if file.IsDir {
+						if err := repos.Fs.CreateDir(file.Path); err != nil {
+							log.Fatalf("Error: %s", err.Error())
+						}
+					} else {
+						res, err := http.Get(fmt.Sprintf("http://%s/files/%s", host, file.Path))
+						if err != nil {
+							log.Fatalf("Error: %s", err.Error())
+						}
+						defer res.Body.Close()
+						fmt.Printf("status(%s): %s\n", res.Request.URL, res.Status)
+						fbytes, err := io.ReadAll(res.Body)
+						if err != nil {
+							log.Fatalf("Error: %s", err.Error())
+						}
+						if err := repos.Fs.Create(file.Path, fbytes); err != nil {
+							log.Fatalf("Error: %s", err.Error())
+						}
+					}
 				}
 				return
 			}
