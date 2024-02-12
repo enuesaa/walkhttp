dcd4ac90b88f4f5fb4a19d9889420496c6c6d28f
nua <enuesaa@gmail.com>
2024-02-12 17:09:28 +0900

    feat(get): cerate get command

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/charmbracelet/bubbles v0.16.1 // indirect
 	github.com/charmbracelet/bubbletea v0.24.2 // indirect
 	github.com/charmbracelet/lipgloss v0.7.1 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -25,6 +26,7 @@ require (
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
--- pkg/cli/get.go
+++ pkg/cli/get.go
@@ -1,7 +1,11 @@
 package cli
 
 import (
+	"fmt"
+
+	"github.com/enuesaa/walkin/pkg/invoke"
 	"github.com/enuesaa/walkin/pkg/repository"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -9,9 +13,33 @@ func CreateGetCmd(repos repository.Repos) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "get",
 		Short: "get request",
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			url, _ := cmd.Flags().GetString("url")
+			if url == "" {
+				prompt := promptui.Prompt{
+					Label: "url",
+				}
+				result, err := prompt.Run()
+				if err != nil {
+					return err
+				}
+				url = result
+			}
+
+			invocation := invoke.Invocation {
+				Method: "GET",
+				Url: url,
+			}
+			if err := invoke.Invoke(&invocation); err != nil {
+				return err
+			}
+			fmt.Printf("status: %d\n", invocation.Status)
+			fmt.Printf("body: %s\n", string(invocation.ResponseBody))
+
+			return nil
 		},
 	}
+	cmd.Flags().String("url", "", "url")
 
 	return cmd
 }
