deefd1e99eb42883e7375ea88ca276ef88b3caa4
nua <enuesaa@gmail.com>
2024-03-09 19:55:40 +0900

    feat(up): serve ctlweb

--- pkg/cli/up.go
+++ pkg/cli/up.go
@@ -1,13 +1,10 @@
 package cli
 
 import (
-	"fmt"
-
-	"github.com/enuesaa/walkin/pkg/conf"
+	"github.com/enuesaa/walkin/ctlweb"
 	"github.com/enuesaa/walkin/pkg/repository"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/proxy"
 	"github.com/spf13/cobra"
 )
 
@@ -16,19 +13,21 @@ func CreateUpCmd(repos repository.Repos) *cobra.Command {
 		Use:   "up",
 		Short: "up",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			config, err := conf.Read(repos)
-			if err != nil {
-				return err
-			}
+			// config, err := conf.Read(repos)
+			// if err != nil {
+			// 	return err
+			// }
 
 			app := fiber.New()
 			app.Use(logger.New())
-		
-			app.Get("/*", func(c *fiber.Ctx) error {
-				path := config.BaseUrl + c.OriginalURL()
-				fmt.Printf("path: %s\n", path)
-				return proxy.Do(c, path)
-			})
+
+			app.All("/*", ctlweb.Serve)
+	
+			// app.Get("/*", func(c *fiber.Ctx) error {
+			// 	path := config.BaseUrl + c.OriginalURL()
+			// 	fmt.Printf("path: %s\n", path)
+			// 	return proxy.Do(c, path)
+			// })
 		
 			return app.Listen(":3000")
 		},
