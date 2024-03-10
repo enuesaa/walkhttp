1170f1dbe316c4424638cb0c49e32949e75558b7
nua <enuesaa@gmail.com>
2024-03-10 20:03:54 +0900

    refactor: send message

--- pkg/cli/up.go
+++ pkg/cli/up.go
@@ -1,19 +1,39 @@
 package cli
 
 import (
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/enuesaa/walkin/ctlweb"
+	"github.com/enuesaa/walkin/pkg/conf"
 	"github.com/enuesaa/walkin/pkg/repository"
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
 	"github.com/spf13/cobra"
 )
 
+func NewMessages() Messages {
+	return Messages{
+		items: make([]string, 0),
+	}
+}
+
 type Messages struct {
-	Items []string
+	items []string
+}
+func (m *Messages) Add(item string) {
+	m.items = append(m.items, item)
+}
+func (m *Messages) First() ([]byte, error) {
+	if len(m.items) > 0 {
+		item := m.items[0]
+		m.items = m.items[1:]
+		return []byte(item), nil
+	}
+	return make([]byte, 0), fmt.Errorf("no item")
 }
 
 func CreateUpCmd(repos repository.Repos) *cobra.Command {
@@ -21,46 +41,37 @@ func CreateUpCmd(repos repository.Repos) *cobra.Command {
 		Use:   "up",
 		Short: "up",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// config, err := conf.Read(repos)
-			// if err != nil {
-			// 	return err
-			// }
+			config, err := conf.Read(repos)
+			if err != nil {
+				return err
+			}
 
 			app := fiber.New()
 			app.Use(logger.New())
 
-			messages := Messages{
-				Items: make([]string, 0),
-			}
-
+			messages := NewMessages()
 			app.Get("/ws", websocket.New(func(c *websocket.Conn) {
 				defer func ()  {
 					c.Close()
 					log.Println("closed")					
 				}()
 
-				for range 10 {
+				for {
 					time.Sleep(2 * time.Second)
-					if len(messages.Items) > 0 {
-						c.WriteMessage(websocket.TextMessage, []byte(messages.Items[0]))
-						messages.Items = messages.Items[1:]
+					item, err := messages.First()
+					if err != nil {
+						continue
 					}
+					c.WriteMessage(websocket.TextMessage, item)
 				}
 			}))
-
 			app.All("/api/*", func(c *fiber.Ctx) error {
-				messages.Items = append(messages.Items, c.Path())
-				return nil
+				path := fmt.Sprintf("%s%s", config.BaseUrl, c.OriginalURL())
+				messages.Add(path)
+				return proxy.Do(c, path)
 			})
-
 			app.All("/*", ctlweb.Serve)
 	
-			// app.Get("/*", func(c *fiber.Ctx) error {
-			// 	path := config.BaseUrl + c.OriginalURL()
-			// 	fmt.Printf("path: %s\n", path)
-			// 	return proxy.Do(c, path)
-			// })
-		
 			return app.Listen(":3000")
 		},
 	}
