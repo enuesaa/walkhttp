eafeb48fcc529735162a928b38d416d6e201ff06
nua <enuesaa@gmail.com>
2024-04-14 20:08:15 +0900

    feat: try subscription with fiber but it does not work. almost because of websocket connectioning

--- pkg/serve/main.go
+++ pkg/serve/main.go
@@ -2,13 +2,18 @@ package serve
 
 import (
 	"fmt"
-	"net/http"
 
+	"github.com/enuesaa/walkin/ctlweb"
 	"github.com/enuesaa/walkin/pkg/repository"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/enuesaa/walkin/pkg/graph"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
 
 func New(repos repository.Repos) Servectl {
@@ -30,13 +35,20 @@ func (s *Servectl) Addr() string {
 }
 
 func (s *Servectl) Listen() error {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
+	
 	// see https://github.com/99designs/gqlgen/issues/1664
 	gqhandle := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
 		Resolvers: &graph.Resolver{},
 	}))
 	gqhandle.AddTransport(&transport.Websocket{})
-	http.Handle("/graph/playground", playground.Handler("graph", "/graph"))
-	http.Handle("/graph", gqhandle)
+	app.All("/graph", adaptor.HTTPHandlerFunc(gqhandle.ServeHTTP))
+	app.Get("/graph/playground", adaptor.HTTPHandlerFunc(playground.Handler("graph", "/graph")))
+	app.All("/*", ctlweb.Serve)
 
-	return http.ListenAndServe(s.Addr(), nil)
+	return app.Listen(s.Addr())
 }
