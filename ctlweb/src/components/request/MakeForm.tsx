0990ecc39e96c1bc8a72191b63dd653d092253b1
nua <enuesaa@gmail.com>
2024-06-08 19:44:14 +0900

    feat(ctlweb): switch call place based on form value.

--- ctlweb/src/components/request/MakeForm.tsx
+++ ctlweb/src/components/request/MakeForm.tsx
@@ -2,27 +2,44 @@ import { Select, TextField, Button, SegmentedControl } from '@radix-ui/themes'
 import styles from './MakeForm.css'
 import { useMakeServerInvocation } from '@/graph/make-server-invocation'
 import { useMakeBrowserInvocation } from '@/graph/make-browser-invocation'
-import { FormEventHandler, MouseEventHandler } from 'react'
+import { FormEventHandler, MouseEventHandler, useState } from 'react'
 
 export const MakeForm = () => {
   const [invoveServerData, invokeServer] = useMakeServerInvocation()
   const [invoveBrowserData, invokeBrowser] = useMakeBrowserInvocation()
+  const [fromServer, setFromServer] = useState<boolean>(true)
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
-    const invocation = {
-      method: (e.currentTarget.elements.namedItem('method') as HTMLInputElement).value,
-      url: (e.currentTarget.elements.namedItem('url') as HTMLInputElement).value,
-      requestHeaders: [],
-      requestBody: '',
+
+    if (fromServer) {
+      const invocation = {
+        method: (e.currentTarget.elements.namedItem('method') as HTMLInputElement).value,
+        url: (e.currentTarget.elements.namedItem('url') as HTMLInputElement).value,
+        requestHeaders: [],
+        requestBody: '',
+      }
+      await invokeServer({ invocation })
+    } else {
+      const invocation = {
+        method: (e.currentTarget.elements.namedItem('method') as HTMLInputElement).value,
+        url: (e.currentTarget.elements.namedItem('url') as HTMLInputElement).value,
+        requestHeaders: [],
+        requestBody: '',
+      }
+      // call here.
+      await invokeBrowser({ invocation })
     }
-    await invokeServer({ invocation })
+  }
+
+  const handleChangeFrom = (from: string) => {
+    setFromServer(from === 'Server')
   }
 
   return (
     <form className={styles.main} onSubmit={handleSubmit}>
       <div className={styles.from}>
-        <SegmentedControl.Root defaultValue='Server' size='3' radius='full'>
+        <SegmentedControl.Root defaultValue='Server' size='3' radius='full' onValueChange={handleChangeFrom}>
           <SegmentedControl.Item value='Server'>Server</SegmentedControl.Item>
           <SegmentedControl.Item value='Browser'>Browser</SegmentedControl.Item>
         </SegmentedControl.Root>
