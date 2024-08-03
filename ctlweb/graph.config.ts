b3ab42b71623b63ff616dfa65f455dbda6ec73ee
nua <enuesaa@gmail.com>
2024-08-03 17:49:10 +0900

    feat(ctlweb): `@graphql-codegen/cli` で documents を指定してみる

--- ctlweb/graph.config.ts
+++ ctlweb/graph.config.ts
@@ -3,10 +3,13 @@ import { type CodegenConfig } from '@graphql-codegen/cli'
 export default {
   overwrite: true,
   schema: '../pkg/serve/schema/*.graphql',
+  documents: './src/gqlqueries/*.ts',
   generates: {
     './src/graph/types.ts': {
       plugins: ['typescript'],
     },
   },
-  hooks: { afterAllFileWrite: ['prettier --write'] },
+  hooks: {
+    afterAllFileWrite: ['prettier --write'],
+  },
 } satisfies CodegenConfig
---
+++ ctlweb/src/gqlqueries/appConfig.ts
@@ -0,0 +1,12 @@
+import { gql, useQuery } from 'urql'
+import { Query } from '../graph/types'
+
+const query = gql`
+  query {
+    appConfig {
+      baseUrl
+    }
+  }
+`
+
+export const useGetAppConfig = () => useQuery<Query>({ query })[0]
