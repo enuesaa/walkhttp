5b9622b443f1dd64b559390780cfff4b90ac1b00
nua <enuesaa@gmail.com>
2024-08-04 17:00:27 +0900

    feat(workspace): define entry struct.

--- internal/invoke/workspace.go
+++ internal/invoke/workspace.go
@@ -8,5 +8,25 @@ func NewConfig() Workspace {
 
 type Workspace struct {
 	BaseUrl string `json:"baseUrl"` // like `https://example.com`
-	// Histories []Invocation `json:"histories"`
+	// Entries []Entry `json:"entries"`
+}
+
+type Entry struct {
+	Request EntryRequest `json:"request"`
+	Response EntryResponse `json:"response"`
+}
+
+type EntryRequest struct {
+	Method  string `json:"method"`
+	Url     string `json:"url"`
+	Headers map[string][]string `json:"headers"`
+	Body    string `json:"body"`
+	Started int    `json:"started"`
+}
+
+type EntryResponse struct {
+	Status  int `json:"status"`
+	Headers map[string][]string `json:"headers"`
+	Body    string `json:"body"`
+	Received int `json:"received"`
 }
