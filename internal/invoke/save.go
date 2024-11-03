dd1694feeb6ce14803822e213341752e14523be1
nua <enuesaa@gmail.com>
2024-11-03 19:52:47 +0900

    fix(save): create parent dir to save http request results.

--- internal/invoke/save.go
+++ internal/invoke/save.go
@@ -1,9 +1,17 @@
 package invoke
 
-import "github.com/google/uuid"
+import (
+	"path/filepath"
+
+	"github.com/google/uuid"
+)
 
 func (srv *InvokeSrv) Save(entry Entry) error {
 	if !srv.repos.Fs.IsExist(srv.repos.WorkspacePath) {
+		basedir := filepath.Dir(srv.repos.WorkspacePath)
+		if err := srv.repos.Fs.CreateDir(basedir); err != nil {
+			return err
+		}
 		ws := Workspace{}
 		if err := srv.Write(ws); err != nil {
 			return err
