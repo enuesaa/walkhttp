a1fdb74d4b16cec27bf389492ba3fcec07c36d2c
nua <enuesaa@gmail.com>
2024-11-04 19:55:06 +0900

    chore: echo の proxy middleware を触ってみたが http request の細かい override など難しそうなので、従来の想定通り AWS API Gateway で言う http (非プロキシ) 方式で通信する

--- internal/serve/serve.go
+++ internal/serve/serve.go
@@ -1,6 +1,8 @@
 package serve
 
 import (
+	// "net/url"
+
 	"github.com/enuesaa/walkhttp/ctlweb"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -19,6 +21,18 @@ func (ctl *ServeCtl) Serve() error {
 	app.GET("/graphql/playground", ctl.handleGqlPlayground())
 	app.Any("/*", ctlweb.Serve)
 	app.Any("/", ctlweb.Serve)
+
+	// proxy
+	// proxyroot := app.Group("/proxy")
+	// exampleSite, err := url.Parse("https://example.com")
+	// if err != nil {
+	// 	return err
+	// }
+	// proxyroot.Use(middleware.ProxyWithConfig(middleware.ProxyConfig{
+	// 	Balancer: middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{
+	// 		{URL: exampleSite},
+	// 	}),
+	// }))
 
 	app.HideBanner = true
 	app.HidePort = true
