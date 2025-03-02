b954b94db75a23a21389f786191967414b8d3637
nua <enuesaa@gmail.com>
2025-03-02 19:49:47 +0900

    fix(cli): invalid port number displayed.

--- internal/command/banner.go
+++ internal/command/banner.go
@@ -2,16 +2,16 @@ package command
 
 import "github.com/enuesaa/walkhttp/internal/repository"
 
-func printBanner(repos repository.Repos) {
-	// print planning like fiber v2 message
+func printBanner(repos repository.Repos, port int) {
 	repos.Log.Printf("┌─────────────────────────────────────────────────────────────────\n")
 	repos.Log.Printf("│ walkhttp\n")
 	repos.Log.Printf("│\n")
 	repos.Log.Printf("│ --origin %s \n", repos.Env.WALKHTTP_URL_BASE)
-	repos.Log.Printf("│ --port   3000 \n")
+	repos.Log.Printf("│ --port   %d \n", port)
 	repos.Log.Printf("│\n")
-	repos.Log.Printf("│ Web console: http://localhost:3000/_\n")
+	repos.Log.Printf("│ Web console: http://localhost:%d/_\n", port)
 	repos.Log.Printf("└─────────────────────────────────────────────────────────────────\n")
 	repos.Log.Printf("\n")
-	repos.Log.Printf(" Try `curl http://localhost:3000/` and open the web console.\n")
+	repos.Log.Printf(" Open the web console and try `curl http://localhost:%d/`\n", port)
+	repos.Log.Printf("\n")
 }
--- internal/command/main.go
+++ internal/command/main.go
@@ -12,7 +12,7 @@ import (
 func New(repos repository.Repos) *cli.App {
 	app := &cli.App{
 		Name:    "walkhttp",
-		Version: "0.0.10",
+		Version: "0.0.11",
 		Usage:   "A CLI tool to proxy HTTP requests for debugging.",
 		Flags: []cli.Flag{
 			&cli.IntFlag{
@@ -37,7 +37,7 @@ func New(repos repository.Repos) *cli.App {
 			if origin != "" {
 				repos.Env.WALKHTTP_URL_BASE = origin
 			}
-			printBanner(repos)
+			printBanner(repos, port)
 
 			if prompt {
 				go func () {
