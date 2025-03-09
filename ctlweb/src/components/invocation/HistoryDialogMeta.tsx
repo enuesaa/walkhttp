aca34ef1b3a846ae87fd5c682704715317df7c32
nua <enuesaa@gmail.com>
2025-03-09 18:09:36 +0900

    feat(ctlweb): styling of card

--- ctlweb/src/components/invocation/HistoryDialogMeta.tsx
+++ ctlweb/src/components/invocation/HistoryDialogMeta.tsx
@@ -26,7 +26,17 @@ export const HistoryDialogMeta = ({ invocation }: Props) => {
         <div className='bg-stone-900 flex-auto px-2 text-left'>{invocation.url}</div>
       </section>
 
-      <div className='text-sm text-stone-400 absolute'>{started}</div>
+      <div className='py-2' style={{
+        backgroundImage: 'radial-gradient(#57534e 1px, transparent 1px)',
+        backgroundSize: '20px 20px',
+      }}>
+        <div className='bg-stone-800 rounded-lg w-fit min-w-24 h-[50px] mx-3 px-1 text-sm border border-stone-600 text-stone-400'>
+          <span className='font-semibold'>Started</span>
+          <div className='w-full text-center text-stone-300 mt-[2px]'>
+            {started}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
